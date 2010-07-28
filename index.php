9d708c542a434b676745ff4da63ac84a164fbc0d
Daniel Bergey <dbergey@gmail.com>
2010-07-28 09:40:37 -0400

    New google analytics code.

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 	<script type="text/javascript">
 
 	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-131389-1']);
+	  _gaq.push(['_setAccount', 'UA-17675528-1']);
 	  _gaq.push(['_trackPageview']);
 
 	  (function() {
@@ -85,6 +85,5 @@
 	  })();
 
 	</script>
-	
 </body>
 </html>
